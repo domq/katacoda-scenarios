035d29dab6798cd399d51c25027b43d5949ebf6d
Dominique Quatravaux <dominique@quatravaux.org>
2019-06-20 14:09:51 +0200

    Rince tutorial/docker dès le début (perturbant)

--- meteor-vue2/scripts/prep/en-tant-que-scrapbook.sh
+++ meteor-vue2/scripts/prep/en-tant-que-scrapbook.sh
@@ -2,4 +2,3 @@
 
 chown -R scrapbook /home/scrapbook/.meteor || true
 chown -R scrapbook /home/scrapbook/.config || true
-rm -rf /home/scrapbook/tutorial/docker
---
+++ meteor-vue2/scripts/prep/rm-rf-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -rf /home/scrapbook/tutorial/docker
