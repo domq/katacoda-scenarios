4106bfc1bf566cd3c054447404f9a5c71de55a8b
Dominique Quatravaux <dominique@quatravaux.org>
2019-06-21 14:23:05 +0200

    Plus de vérification à la fin de 06-vue.md

--- meteor-vue2/scripts/verification/plus-de-blaze.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e -x
-exec 2>> /tmp/check.log
-
-grep ^akryum:vue-component /home/scrapbook/tutorial/.meteor/packages
-! grep ^blaze-html-templates /home/scrapbook/tutorial/.meteor/packages
-echo done
