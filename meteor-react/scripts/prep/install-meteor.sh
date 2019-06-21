31c87f7c947fd1ee3533682f9169181b01cfb26b
Dominique Quatravaux <dominique@quatravaux.org>
2019-06-21 18:17:35 +0200

    Cosmétique : rm -rf docker aussitôt que possible
    Cela empêche «docker» d'apparaître dans le navigateur de fichiers

--- meteor-react/scripts/prep/install-meteor.sh
+++ meteor-react/scripts/prep/install-meteor.sh
@@ -1,2 +1,3 @@
+rm -rf /home/scrapbook/tutorial/docker
 curl https://install.meteor.com/ | env HOME=/root sh -x
 su -l scrapbook
--- meteor-vue2/scripts/prep/install-meteor.sh
+++ meteor-vue2/scripts/prep/install-meteor.sh
@@ -1,3 +1,4 @@
+rm -rf /home/scrapbook/tutorial/docker
 curl https://install.meteor.com/ | env HOME=/root sh -x
 su -l scrapbook
 rm -rf tutorial
